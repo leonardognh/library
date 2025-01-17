6ff49d5773b2611d6a5b78c2a3bbe454831d18d4
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-01-16 23:36:47 -0300

    trying deploy vercel

--- src/server.ts
+++ src/server.ts
@@ -33,5 +33,5 @@ app.get("/", (req, res) => {
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/swagger`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
